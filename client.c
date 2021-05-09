3a219bd6bf208330bc5a65916d58461bc69ad44d
mlryckman27 <mlryckman27@gmail.com>
2021-05-09 13:46:16 -0400

    Created main source files.

---
+++ client.c
@@ -0,0 +1,16 @@
+/* A simple chat client for sending 
+ * messages to other chat clients who
+ * are connected to the main server.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+
+
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,15 @@
+/* A simple server for handling incoming
+ * connections from chat clients
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+	
+
+	return 0;
+}
